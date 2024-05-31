50ccf0dd187354602ece9ad43e874598e10f8160
macbook <metehanakgul@ogr.trakya.edu.tr>
2024-05-31 03:55:12 +0300

    Made changes in rest_client
    -'package:http/http.dart' import was removed and 'package:edirne_gezgini_ui/util/http_request/http_request_type.dart' import was added
    -'Content-Type': 'application/json' was added to headers variable
    -body field in http.put and http.post is now turned into json format with jsonEncode(clientEntity.body)

--- lib/util/http_request/rest_client.dart
+++ lib/util/http_request/rest_client.dart
@@ -3,11 +3,9 @@ import 'dart:io';
 import 'package:edirne_gezgini_ui/model/api_response.dart';
 import 'package:edirne_gezgini_ui/util/http_request/abstract_rest_client.dart';
 import 'package:edirne_gezgini_ui/util/http_request/client_entity.dart';
-import 'package:http/http.dart';
+import 'package:edirne_gezgini_ui/util/http_request/http_request_type.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-
-import 'http_request_type.dart';
 
 class RestClient extends AbstractRestClient {
   @override
@@ -17,29 +15,32 @@ class RestClient extends AbstractRestClient {
     } catch (e) {
       return APIResponse(
           httpStatus: HttpStatus.internalServerError,
-          message: "error while sending request");
+          message: "error while sending request 5892");
     }
   }
 
   Future<APIResponse> _sendRequest(ClientEntity clientEntity) async {
     String? token = clientEntity.token;
+    final body = jsonEncode(clientEntity.body);
     HttpRequestType requestType = clientEntity.requestType;
     Uri uri = Uri.parse(clientEntity.url);
-    Map<String, String> headers = {'Authorization': "Bearer $token"};
+    Map<String, String> headers = {
+      'Authorization': "Bearer $token",
+      'Content-Type': 'application/json'
+    };
     late http.Response response;
 
     switch (requestType) {
-      case post:
+      case HttpRequestType.put:
+        response = await http.put(uri, body: body, headers: headers);
+
+      case HttpRequestType.post:
         response = clientEntity.token == null
             //if that is a authentication request, token will not be need.
-            ? await http.post(uri, body: clientEntity.body)
-            : await http.post(uri, body: clientEntity.body, headers: headers);
+            ? await http.post(uri, body: body, headers: {'Content-Type': 'application/json'})
+            : await http.post(uri, body: body, headers: headers);
 
-      case put:
-        response =
-            await http.put(uri, body: clientEntity.body, headers: headers);
-
-      case delete:
+      case HttpRequestType.delete:
         response = await http.delete(uri, headers: headers);
 
       default:
