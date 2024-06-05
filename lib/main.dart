86ec6481f3df87f6442fd5bc07e01e2c95ecab41
macbook <metehanakgul@ogr.trakya.edu.tr>
2024-06-05 17:03:38 +0300

    DependencyInjector was added
    -DependencyInjector which will use get_it library and create singleton for dependencies was added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:edirne_gezgini_ui/util/dependency_injector.dart';
 import 'package:edirne_gezgini_ui/view/bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 
 void main() {
+  DependencyInjector.setupDependencies();
   runApp(const BottomNavBar());
 }
 
